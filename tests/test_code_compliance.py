17892e7e8e610a660d8a48073aa26db6c78d77b2
Richie Cahill <Richie@tmmworkshop.com>
2025-05-16 18:02:41 -0400

    updated test_code_compliance.py

--- tests/test_code_compliance.py
+++ tests/test_code_compliance.py
@@ -7,14 +7,14 @@ from system_tools.common import bash_wrapper
 
 def test_ruff_check() -> None:
     """Test ruff check."""
-    stdout, returncode = bash_wrapper("poetry run ruff check .")
+    stdout, returncode = bash_wrapper("uv run ruff check .")
     assert stdout == "All checks passed!\n"
     assert returncode == 0
 
 
 def test_ruff_format() -> None:
     """Test ruff format."""
-    stdout, returncode = bash_wrapper("poetry run ruff format --check .")
+    stdout, returncode = bash_wrapper("uv run ruff format --check .")
     test = stdout.strip()
     assert match(r"[\d]* files already formatted", test)
     assert returncode == 0
@@ -22,7 +22,7 @@ def test_ruff_format() -> None:
 
 def test_mypy_check() -> None:
     """Test mypy check."""
-    stdout, returncode = bash_wrapper("poetry run mypy .")
+    stdout, returncode = bash_wrapper("uv run mypy .")
     test = stdout.strip()
     assert match(r"Success: no issues found in [\d]* source files", test)
     assert returncode == 0
