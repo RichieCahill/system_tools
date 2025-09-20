4b19e7f8b76d18e4a84b596931426fc13d7c9085
Richie Cahill <Richie@tmmworkshop.com>
2025-09-20 14:31:05 -0400

    updated validate_system.py with typer

--- tests/test_server_validate_scripts.py
+++ tests/test_server_validate_scripts.py
@@ -1,79 +1,60 @@
 """test_server_validate_scripts."""
 
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
 import pytest
 from pytest_mock import MockerFixture
 
 from system_tools.system_tests.validate_system import main
 
-SCRIPT_PATH = "system_tools.system_tests.validate_system"
+if TYPE_CHECKING:
+    from pyfakefs.fake_filesystem import FakeFilesystem
+    from pytest_mock import MockerFixture
+
+VALIDATE_SYSTEM = "system_tools.system_tests.validate_system"
 
 
-def test_validate_system(mocker: MockerFixture) -> None:
+def test_validate_system(mocker: MockerFixture, fs: FakeFilesystem) -> None:
     """test_validate_system."""
-    mocker.patch(f"{SCRIPT_PATH}.ArgumentParser")
-    mocker.patch(
-        f"{SCRIPT_PATH}.load_config_data",
-        return_value={
-            "zpools": ["media", "storage"],
-            "services": ["docker"],
-        },
+    fs.create_file(
+        "/mock_snapshot_config.toml",
+        contents='zpool = ["root_pool", "storage", "media"]\nservices = ["docker"]\n',
     )
-    mock_discord_notification = mocker.patch(f"{SCRIPT_PATH}.discord_notification", return_value=None)
 
-    mocker.patch(f"{SCRIPT_PATH}.systemd_tests", return_value=None)
-    mocker.patch(f"{SCRIPT_PATH}.zpool_tests", return_value=None)
-
-    main()
-
-    assert mock_discord_notification.call_count == 0
+    mocker.patch(f"{VALIDATE_SYSTEM}.systemd_tests", return_value=None)
+    mocker.patch(f"{VALIDATE_SYSTEM}.zpool_tests", return_value=None)
+    main("foo", Path("/mock_snapshot_config.toml"))
 
 
-def test_validate_system_errors(mocker: MockerFixture) -> None:
+def test_validate_system_errors(mocker: MockerFixture, fs: FakeFilesystem) -> None:
     """test_validate_system_errors."""
-    mocker.patch(f"{SCRIPT_PATH}.ArgumentParser")
-    mocker.patch(
-        f"{SCRIPT_PATH}.load_config_data",
-        return_value={
-            "zpools": ["media", "storage"],
-            "services": ["docker"],
-        },
+    fs.create_file(
+        "/mock_snapshot_config.toml",
+        contents='zpool = ["root_pool", "storage", "media"]\nservices = ["docker"]\n',
     )
-    mock_discord_notification = mocker.patch(f"{SCRIPT_PATH}.discord_notification", return_value=None)
-    mocker.patch(f"{SCRIPT_PATH}.systemd_tests", return_value=["systemd_tests error"])
-    mocker.patch(f"{SCRIPT_PATH}.zpool_tests", return_value=["zpool_tests error"])
+
+    mocker.patch(f"{VALIDATE_SYSTEM}.systemd_tests", return_value=["systemd_tests error"])
+    mocker.patch(f"{VALIDATE_SYSTEM}.zpool_tests", return_value=["zpool_tests error"])
 
     with pytest.raises(SystemExit) as exception_info:
-        main()
+        main("foo", Path("/mock_snapshot_config.toml"))
 
     assert exception_info.value.code == 1
 
-    assert mock_discord_notification.call_count == 1
 
-
-def test_validate_system_execution(mocker: MockerFixture) -> None:
+def test_validate_system_execution(mocker: MockerFixture, fs: FakeFilesystem) -> None:
     """test_validate_system_execution."""
-    mocker.patch(f"{SCRIPT_PATH}.ArgumentParser")
-    mocker.patch(
-        f"{SCRIPT_PATH}.load_config_data",
-        return_value={
-            "zpools": ["media", "storage"],
-            "services": ["docker"],
-        },
+    fs.create_file(
+        "/mock_snapshot_config.toml",
+        contents='zpool = ["root_pool", "storage", "media"]\nservices = ["docker"]\n',
     )
-    mock_discord_notification = mocker.patch(f"{SCRIPT_PATH}.discord_notification", return_value=None)
-    mocker.patch(f"{SCRIPT_PATH}.zpool_tests", side_effect=RuntimeError("zpool_tests error"))
+
+    mocker.patch(f"{VALIDATE_SYSTEM}.zpool_tests", side_effect=RuntimeError("zpool_tests error"))
 
     with pytest.raises(SystemExit) as exception_info:
-        main()
+        main("foo", Path("/mock_snapshot_config.toml"))
 
     assert exception_info.value.code == 1
-
-    assert mock_discord_notification.call_count == 1
-
-
-def test_validate_system_no_config_file() -> None:
-    """test_validate_system_execution."""
-    with pytest.raises(FileNotFoundError) as exception_info:
-        main()
-
-    assert exception_info.value.args[0] == "Config file config.toml does not exist"
